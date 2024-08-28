d656e8b4a6f21ee418b1580f84570e4c04d8baa2
Ilya Baldin <baldin@jlab.org>
2024-08-28 15:17:09 -0400

    Updated reassembler getEvent API

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -3,7 +3,7 @@ var searchData=
   ['read_5ffile_0',['read_file',['../namespacee2sar.html#a830f33137fb76ad3dac564a61bdfecf0',1,'e2sar']]],
   ['reassembler_1',['Reassembler',['../classe2sar_1_1_reassembler.html',1,'e2sar::Reassembler'],['../classe2sar_1_1_reassembler.html#a59944e548b624c5e5e65f3fda6496485',1,'e2sar::Reassembler::Reassembler(const EjfatURI &amp;uri, std::vector&lt; int &gt; cpuCoreList, const ReassemblerFlags &amp;rflags=ReassemblerFlags())'],['../classe2sar_1_1_reassembler.html#ae3242ab50c3675511cf6a6922dfe2248',1,'e2sar::Reassembler::Reassembler(const EjfatURI &amp;uri, size_t numRecvThreads=1, const ReassemblerFlags &amp;rflags=ReassemblerFlags())']]],
   ['reassemblerflags_2',['ReassemblerFlags',['../structe2sar_1_1_reassembler_1_1_reassembler_flags.html',1,'e2sar::Reassembler']]],
-  ['recvevent_3',['recvEvent',['../classe2sar_1_1_reassembler.html#a8d97b06e8813bf53ade74c08de8f4e91',1,'e2sar::Reassembler']]],
+  ['recvevent_3',['recvEvent',['../classe2sar_1_1_reassembler.html#a15704c5f2d91322390cb0899ff4bbac9',1,'e2sar::Reassembler']]],
   ['registerworker_4',['registerWorker',['../classe2sar_1_1_l_b_manager.html#a71ba78cf2988e9bb7373d707b16c9f35',1,'e2sar::LBManager::registerWorker()'],['../classe2sar_1_1_reassembler.html#a5804969f47c755008cf171c765e3b7e5',1,'e2sar::Reassembler::registerWorker()']]],
   ['rehdr_5',['REHdr',['../structe2sar_1_1_r_e_hdr.html',1,'e2sar']]],
   ['removesenders_6',['removeSenders',['../classe2sar_1_1_l_b_manager.html#ad0f1647dfad7988f277421ea6ffdc9b3',1,'e2sar::LBManager']]],
