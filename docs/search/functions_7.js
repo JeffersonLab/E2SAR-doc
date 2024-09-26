296501b217b8ca7e3e7ed77d80b6a8da8738c5dc
Ilya Baldin <baldin@jlab.org>
2024-09-26 10:11:01 -0400

    Updated docs

--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['makessloptions_0',['makeSslOptions',['../classe2sar_1_1_l_b_manager.html#a895ec804b742e4127688558b43638471',1,'e2sar::LBManager']]],
-  ['makessloptionsfromfiles_1',['makeSslOptionsFromFiles',['../classe2sar_1_1_l_b_manager.html#a5f294de412c832a4a1264fcec64f95bc',1,'e2sar::LBManager::makeSslOptionsFromFiles(std::string_view pem_root_certs, std::string_view pem_private_key, std::string_view pem_cert_chain)'],['../classe2sar_1_1_l_b_manager.html#a90457e1355f2fd6e3a199dff2d021a2a',1,'e2sar::LBManager::makeSslOptionsFromFiles(std::string_view pem_root_certs)']]]
+  ['makessloptions_0',['makeSslOptions',['../classe2sar_1_1_l_b_manager.html#ae5ca586788cfe5af16c4cbed1ade24c1',1,'e2sar::LBManager']]],
+  ['makessloptionsfromfiles_1',['makeSslOptionsFromFiles',['../classe2sar_1_1_l_b_manager.html#a957151ee47888b7c3ceebdc549104f5e',1,'e2sar::LBManager::makeSslOptionsFromFiles(std::string_view pem_root_certs, std::string_view pem_private_key, std::string_view pem_cert_chain) noexcept'],['../classe2sar_1_1_l_b_manager.html#a4d177e12011bd72a8feca9d8c7123765',1,'e2sar::LBManager::makeSslOptionsFromFiles(std::string_view pem_root_certs) noexcept']]]
 ];
