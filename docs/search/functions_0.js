0377b636e612f7b550855e648919d2fb6a6bff8e
Ilya Baldin <baldin@jlab.org>
2025-02-24 15:17:54 -0500

    Updates for auto-detection code

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['addsenders_0',['addSenders',['../classe2sar_1_1_l_b_manager.html#acfeed65fb683514bba5215bd4f0c133c',1,'e2sar::LBManager']]],
-  ['addtosendqueue_1',['addToSendQueue',['../classe2sar_1_1_segmenter.html#a05b3ea895a787044f5d87a4d99128382',1,'e2sar::Segmenter']]],
-  ['aslbstatus_2',['asLBStatus',['../classe2sar_1_1_l_b_manager.html#abc9e35f7326e9c99f92b1df9fa347005',1,'e2sar::LBManager::asLBStatus(std::unique_ptr&lt; LoadBalancerStatusReply &gt; &amp;rep) noexcept'],['../classe2sar_1_1_l_b_manager.html#ae89baf10d4efb21aff807b1782000464',1,'e2sar::LBManager::asLBStatus(const LoadBalancerStatusReply &amp;rep) noexcept']]],
-  ['asoverviewmessage_3',['asOverviewMessage',['../classe2sar_1_1_l_b_manager.html#aaf72784069d43e77a52ca30f0fbf438f',1,'e2sar::LBManager::asOverviewMessage(std::unique_ptr&lt; OverviewReply &gt; &amp;rep) noexcept'],['../classe2sar_1_1_l_b_manager.html#acc3efd1ce339180e87d8ea9fd01ddd01',1,'e2sar::LBManager::asOverviewMessage(const OverviewReply &amp;rep) noexcept']]],
-  ['availableasstrings_4',['availableAsStrings',['../classe2sar_1_1_optimizations.html#ae7e9237ab0785345d96f016f4385ca44',1,'e2sar::Optimizations']]],
-  ['availableasword_5',['availableAsWord',['../classe2sar_1_1_optimizations.html#aca7a49dc4e3fb4a45c6f4132e8c62464',1,'e2sar::Optimizations']]]
+  ['addsenderself_1',['addSenderSelf',['../classe2sar_1_1_l_b_manager.html#ab9a2d2c585dbc70e3a91736830d0acd1',1,'e2sar::LBManager']]],
+  ['addtosendqueue_2',['addToSendQueue',['../classe2sar_1_1_segmenter.html#a05b3ea895a787044f5d87a4d99128382',1,'e2sar::Segmenter']]],
+  ['aslbstatus_3',['asLBStatus',['../classe2sar_1_1_l_b_manager.html#abc9e35f7326e9c99f92b1df9fa347005',1,'e2sar::LBManager::asLBStatus(std::unique_ptr&lt; LoadBalancerStatusReply &gt; &amp;rep) noexcept'],['../classe2sar_1_1_l_b_manager.html#ae89baf10d4efb21aff807b1782000464',1,'e2sar::LBManager::asLBStatus(const LoadBalancerStatusReply &amp;rep) noexcept']]],
+  ['asoverviewmessage_4',['asOverviewMessage',['../classe2sar_1_1_l_b_manager.html#aaf72784069d43e77a52ca30f0fbf438f',1,'e2sar::LBManager::asOverviewMessage(std::unique_ptr&lt; OverviewReply &gt; &amp;rep) noexcept'],['../classe2sar_1_1_l_b_manager.html#acc3efd1ce339180e87d8ea9fd01ddd01',1,'e2sar::LBManager::asOverviewMessage(const OverviewReply &amp;rep) noexcept']]],
+  ['availableasstrings_5',['availableAsStrings',['../classe2sar_1_1_optimizations.html#ae7e9237ab0785345d96f016f4385ca44',1,'e2sar::Optimizations']]],
+  ['availableasword_6',['availableAsWord',['../classe2sar_1_1_optimizations.html#aca7a49dc4e3fb4a45c6f4132e8c62464',1,'e2sar::Optimizations']]]
 ];
