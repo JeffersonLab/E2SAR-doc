643604de546bbaea8d1cd5fc986f4b2e493462ae
Ilya Baldin <baldin@jlab.org>
2026-09-06 17:17:18 -0400

    doc: Updating documentation for 0.4.0

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['integersample_0',['IntegerSample',['../structe2sar_1_1_integer_sample.html',1,'e2sar']]],
   ['is_5ferror_5fcode_5fenum_3c_20e2sar_3a_3ae2sarerrorc_20_3e_1',['is_error_code_enum&lt; e2sar::E2SARErrorc &gt;',['../structstd_1_1is__error__code__enum_3_01e2sar_1_1_e2_s_a_r_errorc_01_4.html',1,'std']]],
-  ['isselected_2',['isSelected',['../classe2sar_1_1_optimizations.html#ab43b3d95270136998b351361e6b830ec',1,'e2sar::Optimizations']]],
-  ['isusingipv6_3',['isUsingIPv6',['../classe2sar_1_1_segmenter.html#a30bd5b50187de3cc594f7e83013e94ab',1,'e2sar::Segmenter']]]
+  ['isnonroutable_2',['isNonRoutable',['../classe2sar_1_1_net_util.html#a402d5ccfc4f6e9a1e4e080889ac2e1bf',1,'e2sar::NetUtil::isNonRoutable(const std::string &amp;addr_str) noexcept'],['../classe2sar_1_1_net_util.html#ada2d5ed3eef7e7758c8da47b6a8609db',1,'e2sar::NetUtil::isNonRoutable(const std::vector&lt; std::string &gt; &amp;addrs) noexcept']]],
+  ['isselected_3',['isSelected',['../classe2sar_1_1_optimizations.html#ab43b3d95270136998b351361e6b830ec',1,'e2sar::Optimizations']]],
+  ['isusingipv6_4',['isUsingIPv6',['../classe2sar_1_1_segmenter.html#a30bd5b50187de3cc594f7e83013e94ab',1,'e2sar::Segmenter']]]
 ];
--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['isselected_0',['isSelected',['../classe2sar_1_1_optimizations.html#ab43b3d95270136998b351361e6b830ec',1,'e2sar::Optimizations']]],
-  ['isusingipv6_1',['isUsingIPv6',['../classe2sar_1_1_segmenter.html#a30bd5b50187de3cc594f7e83013e94ab',1,'e2sar::Segmenter']]]
+  ['isnonroutable_0',['isNonRoutable',['../classe2sar_1_1_net_util.html#a402d5ccfc4f6e9a1e4e080889ac2e1bf',1,'e2sar::NetUtil::isNonRoutable(const std::string &amp;addr_str) noexcept'],['../classe2sar_1_1_net_util.html#ada2d5ed3eef7e7758c8da47b6a8609db',1,'e2sar::NetUtil::isNonRoutable(const std::vector&lt; std::string &gt; &amp;addrs) noexcept']]],
+  ['isselected_1',['isSelected',['../classe2sar_1_1_optimizations.html#ab43b3d95270136998b351361e6b830ec',1,'e2sar::Optimizations']]],
+  ['isusingipv6_2',['isUsingIPv6',['../classe2sar_1_1_segmenter.html#a30bd5b50187de3cc594f7e83013e94ab',1,'e2sar::Segmenter']]]
 ];
